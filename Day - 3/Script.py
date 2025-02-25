3c63a0ee83451f308809d81799391a98bc984bbb
Dan1e1B <bernardes.dc@gmail.com>
2025-02-25 13:14:16 +0000

    Day - 3 in progress :->

--- Day - 3/Script.py
+++ Day - 3/Script.py
@@ -1,2 +1,28 @@
-file = open("input", "r")
-strng = file.read()
+file = open("input.txt", "r")
+content = file.read()
+
+CORRECT_STRNG = "mul("
+numbers = 0
+index = 0
+is_strng = False
+is_inputing_number = False
+
+lst = []
+
+
+for letter in content:
+
+    if letter is "m": is_strng = True; index = 1
+    elif is_strng:
+        
+        if (0 < index < 4) and (CORRECT_STRNG[index] != letter): 
+            is_strng = False
+            index = 0
+            continue
+
+        elif (index == 4) or (index == 5):
+            if (letter is ",") and (is_inputing_number == True):
+                is_inputing_number = False
+                
+        index += 1
+        
